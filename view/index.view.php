48549ee233f2748b2eaa2149d31758aa58712846
T0R7AS1 <71092367+T0R7AS1@users.noreply.github.com>
2020-09-17 14:55:08 +0300

    second commit

--- view/index.view.php
+++ view/index.view.php
@@ -2,8 +2,56 @@
 
 use userApp\User;
 
-$user = new User('50114225660','Eividas','Galvanauskas','programuotojas','eividasg7@gmail.com','500','dirbantis');
+for ($i = 1; $i <= 10; $i++){
+
+$user = new User('50114225660', "eividas", "galvanauskas", "programer", "Eividasg7@gmail.com","500","Dirbantis");
+}
 ?>
-    <?php foreach ($user->showProfile() as $item): ?>
-    <?php echo $item .'<br>'?>
-    <?php endforeach ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 10px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #494949;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+
+<h2>Darbuotoju info</h2>
+
+<table>
+    <tr>
+        <th>kodas</th>
+        <th>vardas</th>
+        <th>pavarde</th>
+        <th>pareigos</th>
+        <th>pastas</th>
+        <th>uzmokestis</th>
+        <th>busena</th>
+    </tr>
+    <?php for ($i = 1; $i <= 10; $i++){
+        echo "<tr>";
+        foreach ($user->showProfile() as $item){ ?>
+            <?php echo '<td>'. $item .'</td>';
+        }
+        echo "</tr>";
+    } ?>
+
+</table>
+
+</body>
+</html>
